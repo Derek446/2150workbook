81a77d63958450e7dc5c28ea6e4644addeaf4536
Derek <derek358@icloud.com>
2026-01-29 18:46:44 -0700

    Completed Lesson 3

--- 02 - component communication/exercises/student/src/js/components/resource-results.js
+++ 02 - component communication/exercises/student/src/js/components/resource-results.js
@@ -10,73 +10,118 @@ template.innerHTML = `
       </div>
 
       <div class="list-group list-group-flush">
-        <button type="button" class="list-group-item list-group-item-action active" aria-current="true">
-          <div class="d-flex w-100 justify-content-between">
-            <h2 class="h6 mb-1">Peer Tutoring Centre</h2>
-            <small>Academic</small>
-          </div>
-          <p class="mb-1 small text-body-secondary">Drop-in tutoring and study support.</p>
-          <small class="text-body-secondary">Building W, Room W101</small>
-        </button>
-
-        <button type="button" class="list-group-item list-group-item-action">
-          <div class="d-flex w-100 justify-content-between">
-            <h2 class="h6 mb-1">Counselling Services</h2>
-            <small>Wellness</small>
-          </div>
-          <p class="mb-1 small text-body-secondary">Confidential mental health supports.</p>
-          <small class="text-body-secondary">Virtual and in-person</small>
-        </button>
-
-        <button type="button" class="list-group-item list-group-item-action">
-          <div class="d-flex w-100 justify-content-between">
-            <h2 class="h6 mb-1">Student Awards and Bursaries</h2>
-            <small>Financial</small>
-          </div>
-          <p class="mb-1 small text-body-secondary">Funding options and application help.</p>
-          <small class="text-body-secondary">Student Services, Main Floor CAT</small>
-        </button>
-
-        <button type="button" class="list-group-item list-group-item-action">
-          <div class="d-flex w-100 justify-content-between">
-            <h2 class="h6 mb-1">IT Service Desk</h2>
-            <small>Tech</small>
-          </div>
-          <p class="mb-1 small text-body-secondary">Account access, Wi-Fi, BYOD support.</p>
-          <small class="text-body-secondary">Library</small>
-        </button>
       </div>
     </div>
   </section>`;
 
 class ResourceResults extends HTMLElement {
   // TODO: Create a private field for results data
+  #results = [];
 
   constructor() {
     super();
     // TODO: Bind the handleResultClick method to this instance
-
+    // WTF is this and why do we need to do it? -> https://dev.to/aman_singh/why-do-we-need-to-bind-methods-inside-our-class-component-s-constructor-45bn
+    // If you read to the end, you'll know we could've just used an arrow function... but this illustrates class vs. instance behavioural differences
+    this._handleResultClick = this._handleResultClick.bind(this);
     this.attachShadow({ mode: 'open' });
   }
 
   // TODO: Implement setter for results data, remember to render
+  set results(data) {
+    // The setter method for our private array - similar to what you're learning in C# right now.
+    this.#results = data;
+    this.render(); // when data is set, call our render method (below) to fire the display logic.
+  }
 
   // TODO: Add an event handler method for result selection
+  _handleResultClick(event) {
+    // Let's leave this empty for now,
+    // and first deal with where it needs to be called elsewhere in our object for event-driven behaviour.
+    const button = event.target.closest('button[data-id]');
+    if (button) {
+      // Final touch: highlight the selected element when clicked.
+      // 1) If there was already a currently active button, find it and toggle it off.
+      //    -> Taking the opportunity to show you optional method chaining. Instead of:
+      //         if (possibleItem) { doAThing() }
+      //       we can just:
+      //         possibleItem?.doAThing();
+      this.shadowRoot.querySelector('button.active')?.classList.remove('active');
+      // 2) Slap the active bootstrap class on the clicked button and watch the responsive fanciness happen
+      button.classList.add('active');
+
+      // To prepare click event, first snipe for which item got clicked, so we can grab its data.
+      // -> this is why we always implement a unique identifier for each element in a series!
+      const resultID = button.getAttribute('data-id');
+      const result = this.#results.find(r => r.id === resultID);
+      // note that we're finding the data object from the array, not the UI row!
+
+      // cook up a custom event. docs: https://developer.mozilla.org/en-US/docs/Web/API/CustomEvent/
+      const resultSelectedEvent = new CustomEvent(
+        'resource-selected', // *we* get to decide the event name!
+        {
+          detail: { result }, // don't pre-filter the data item. Send the *whole* object; let the receiving component decide what's relevant.
+          bubbles: true, // if true, parent node / document can listen for event without knowing about & wiring together sender and receiever components
+          composed: true, // if true, events can cross shadow DOM boundary
+        },
+      );
+
+      // broadcast the event to the current target (in this case, a ResourceResults component instance)
+      this.dispatchEvent(resultSelectedEvent);
+    }
+  }
 
   connectedCallback() {
     // TODO: Add a click event listener to handle result selection
-    
+    this.shadowRoot.addEventListener('click', this._handleResultClick);
     this.render();
   }
 
   // TODO: Clean up event listener in disconnectedCallback
-
-  
+  disconnectedCallback() {
+    // <- when the component is unloaded/removed from the DOM...
+    // ... then clean up your unused event listeners!
+    this.shadowRoot.removeEventListener('click', this._handleResultClick);
+  }
 
   render() {
     // TODO: Update to render from the private results field, if it's empty, show "No results found" message
-    
-    this.shadowRoot.appendChild(template.content.cloneNode(true));
+    // copy template HTML above onto the shadow root node for this component,
+    const content = template.content.cloneNode(true);
+    // and snipe that list-group class so we can nest our data display inside it
+    const listGroup = content.querySelector('.list-group');
+
+    // then render a result row for each item in the results array
+    if (this.#results.length) {
+      // resolves true if the results array contains items
+      // for each result in the array, generate HTML to hold/display data
+      // -> pack it all inside a sneaky <button> so we can later easily highlight it when active with bootstrap classes
+      const resultsHTML = this.#results.map(
+        result => `
+        <button type="button" class="list-group-item list-group-item-action" data-id="${result.id}">
+          <div class="d-flex w-100 justify-content-between">
+            <h2 class="h6 mb-1">${result.title}</h2>
+            <small>${result.category}</small>
+          </div>
+          <p class="mb-1 small text-body-secondary">${result.summary}</p>
+          <small class="text-body-secondary">${result.location}</small>
+        </button>`,
+      );
+
+      // inject the HTML we just generated inside the list-group node
+      listGroup.innerHTML = resultsHTML.join(''); // resultsHTML is an array, so combine each HTML blob back-to-back into a string
+    } else {
+      // If #results contains no items, display some default text.
+      // Always communicate to the user in UI design! An empty card might have them wondering if something's broken.
+      listGroup.innerHTML = `
+        <div class="list-group-item">
+          <p class="mb-0">No results found.</p>
+        </div>`;
+    }
+
+    // finally, take the content we composed and add it to the shadow root node!
+    this.shadowRoot.innerHTML = ''; // clear current contents first; we're re-rendering
+    this.shadowRoot.appendChild(content);
   }
 }
 
--- 02 - component communication/exercises/student/src/js/main.js
+++ 02 - component communication/exercises/student/src/js/main.js
@@ -53,5 +53,11 @@ const resultData = [
 ];
 
 // TODO: Pass data to resource-results component
+const resultsComponent = document.querySelector('resource-results');
+resultsComponent.results = resultData; // passes the data to ResourceResults.results setter, since we made the array itself private
 
 // TODO: Listen for resourceSelected event and update resource-details component
+const detailsComponent = document.querySelector('resource-details');
+resultsComponent.addEventListener('resource-selected', (event) => {
+  detailsComponent.resource = event.detail.result;
+});
